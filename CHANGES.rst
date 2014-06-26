50ca148b401c48721da6b56af877a0f38465df35
Alex C. <trebek604@gmail.com>
2014-06-25 17:50:15 -0700

    Lots of changes.
    See CHANGES.rst.

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,23 +2,95 @@
 PyDealer Changelog
 ==================
 
+v1.2.0 (25-06-2014)
+-------------------
+
+Changes
+^^^^^^^
+
+- Moved the constants ``Deck.SUITS``, ``Deck.FACES``, and ``Deck.NUMBERS`` out of ``Deck`` and into the global scope. They can now be accessed using ``SUITS``, ``FACES``, and ``NUMBERS``.
+- Moved ``Deck.check_term`` out of ``Deck``, and into the global scope. It can now be accessed by calling ``check_term``.
+
+Features
+^^^^^^^^
+
+- Added some magic methods to the ``Card``, and ``Deck`` objects.
+    - Added to ``Card``:
+        - ``__eq__``
+        - ``__gt__``
+        - ``__hash__``
+        - ``__repr__``
+        - ``__str__``
+    - Added to ``Deck``:  
+        - ``__add__``
+        - ``__contains__``
+        - ``__delitem__``
+        - ``__eq__``
+        - ``__getitem__``
+        - ``__iter__``
+        - ``__len__``
+        - ``__repr__``
+        - ``__setitem__``
+        - ``__str__``
+- Added a few global constants, for use with some common card games:
+    - ``POKER_RANKS``, which is a dict of poker ranks.
+        - This is also used by ``Card.__gt__``.
+    - ``BIG2_RANKS``, which is a dict of ranks for the game Big Two (Deuces).
+    - ``BLACKJACK_VALS``, which is a dict of card values for Black Jack (Twenty One).
+- ``Deck.peek``, in addition to accepting a single deck indice for an argument, can now accept a list of deck indices as well.
+- ``Deck.get``, in addition to accepting card names/values/suits/abbrevs. as an argument, can now accept deck indices as well.
+
+Refactoring
+^^^^^^^^^^^
+
+- Refactored most of the methods in ``Deck``.
+    - Refactored ``Deck.deal``.
+    - Refactored ``Deck.find``.
+    - Refactored ``Deck.get``.
+- Refactored the methods in ``Card``.
+    - Refactored ``Card.gen_abbrev``.
+    - Refactored ``Card.gen_name``.
+- Refactored ``check_term``
+
+Bugfixes
+^^^^^^^^
+
+- Fixed ``Card.gen_abbrev``, so it now properly abbreviates 10s.
+
+Other
+^^^^^
+
+- Added argument & return value descriptions to the method & function docstrings.
+- Reformatted the changelog, ``CHANGES.rst``.
+
 v1.1.0 (22-06-2014)
-------------------
+-------------------
+
+Changes
+^^^^^^^
 
 - Pydealer is now a proper package, that can be installed/uninstalled using PIP.
+- No longer have to call ``Deck.build_deck`` after instantiating a deck. It it done on initialization now.
 - Shortened a few method names.
     - ``Deck.build_deck`` is now ``Deck.build``.
     - ``Deck.find_cards`` is now ``Deck.find``.
     - ``Deck.get_cards`` is now ``Deck.get``.
+
+Features
+^^^^^^^^
+
 - Decks can now be built with jokers.
     - This can be done by passing the argument ``jokers=True`` to ``Deck``, when instantiating:
         deck = pydealer.Deck(jokers=True)
-- No longer have to call ``Deck.build_deck`` after instantiating a deck. It it done on initialization now.
+
+Other
+^^^^^
+
 - Negligibly optimized a few methods, by replacing some simple loops with list comprehensions.
-- Fixed up the readme. Added a simple usage examples.
-- Fixed up the changelog.
+- Fixed up the readme. Added simple usage examples.
+- Fixed up the changelog, ``CHANGES.rst``.
 
 v1.0.0 (11-05-2014)
-------------------
+-------------------
 
 - Initial release.
--- pydealer/pydealer.py
+++ pydealer/pydealer.py
@@ -1,9 +1,8 @@
 #===============================================================================
-# PyDealer - Card Deck Simulator
+# PyDealer: Playing Card Package
 #-------------------------------------------------------------------------------
-# Version: 1.1.0
-# Updated: 22-06-2014
-# Author: Alex Crawford
+# Version: 1.2.0
+# Updated: 25-06-2014
 # License: MIT
 #===============================================================================
 
@@ -39,33 +38,195 @@ of a given name, abbreviation, value, or suit in the deck.
 # IN THE SOFTWARE.
 
 #===============================================================================
-# IMPORTS
+# Imports
 #===============================================================================
 
 import random
 
 #===============================================================================
-# DECK CLASS
+# Global Constants
+#===============================================================================
+
+SUITS = ["Spades", "Hearts", "Diamonds", "Clubs"]
+FACES = ["King", "Queen", "Jack"]
+NUMBERS = ["2", "3", "4", "5", "6", "7", "8", "9", "10"]
+
+POKER_RANKS = {
+    "Ace": 13,
+    "King": 12,
+    "Queen": 11,
+    "Jack": 10, 
+    "10": 9,
+    "9": 8,
+    "8": 7,
+    "7": 6,
+    "6": 5,
+    "5": 4, 
+    "4": 3,
+    "3": 2,
+    "2": 1
+}
+BIG2_RANKS = {
+    "values": POKER_RANKS,
+    "suits": {
+        "Spades": 4,
+        "Hearts": 3,
+        "Clubs": 2,
+        "Diamonds": 1
+    }
+}
+BLACKJACK_VALS = {
+    "Ace": (1, 11),
+    "King": 10,
+    "Queen": 10,
+    "Jack": 10, 
+    "10": 10,
+    "9": 9,
+    "8": 8,
+    "7": 7,
+    "6": 6,
+    "5": 5, 
+    "4": 4,
+    "3": 3,
+    "2": 2
+}
+
+#===============================================================================
+# Deck Class
 #===============================================================================
 
 class Deck(object):
     """Deck class, representing the deck that the cards will be in."""
 
-    SUITS = ["Spades", "Hearts", "Diamonds", "Clubs"]
-    FACES = ["King", "Queen", "Jack"]
-    NUMBERS = ["2", "3", "4", "5", "6", "7", "8", "9", "10"]
-
-    def __init__(self, jokers=False, num_jokers=2):
+    def __init__(self, jokers=False, num_jokers=2, build=True):
         """Deck init method."""
-
         self.cards = []
         self.decks_used = 0
 
-        self.build(jokers, num_jokers)
+        if build:
+            self.build(jokers, num_jokers)
+
+    def __add__(self, other):
+        """
+        Allows you to add (merge) decks together, with the ``+`` operand.
+
+        :param other: The other deck to add to the deck.
+        :type other: Deck.
+
+        :returns: A new Deck object, with the combined cards.
+
+        """
+        new_deck = Deck(build=False)
+        new_deck.cards = self.cards + other.cards
+        return new_deck
+
+    def __contains__(self, card):
+        """
+        Allows for Card object (not suit & value) inclusion checks.
+
+        :param card: The Card object to check for.
+        :type card: Card.
+
+        :returns: Whether or not the Card is in the Deck.
+
+        """
+        id_list = [id(x) for x in self.cards]
+        return id(item) in id_list
+
+    def __delitem__(self, indice):
+        """
+        Allows for deletion of a Card object, using del.
+
+        :param indice: The indice to delete.
+        :type indice: int.
+
+        """
+        del self.cards[indice]
+
+    def __eq__(self, other):
+        """
+        Allows for Deck ordering comparisons.
+
+        :param other: The other Deck to compare to.
+        :type other: Deck.
+
+        :returns: Whether or not the decks are in the same order.
+
+        """
+        return (isinstance(other, Deck) and self.cards == other.cards)
+
+    def __getitem__(self, indice):
+        """
+        Allows for getting of cards, using ``Deck[indice]``.
+
+        :param indice: The indice to get.
+        :type indice: int.
+
+        :returns: The Card at the given indice.
+
+        """
+        return self.cards[indice]
+
+    def __iter__(self):
+        """
+        Allows looping through the Deck, using for loops.
+
+        :returns: An iterator of the Cards in the deck.
+
+        """
+        for card in self.cards:
+            yield card
+
+    def __len__(self):
+        """
+        Allows check the deck length, with len.
+
+        :returns: The length of the deck (self.cards).
+
+        """
+        return len(self.cards)
+
+    def __repr__(self):
+        """
+        The repr magic method.
+
+        :returns: A string representation of the Deck object.
+
+        """
+        return 'Deck(cards=%r)' % (self.cards)
+
+    def __setitem__(self, indice, value):
+        """
+        Allows you to assign cards to specific deck indices.
+
+        :param indice: The indice to set.
+        :type indice: int.
+        :param value: The Card to set the indice to.
+        :type value: Card.
+
+        :returns: The Card at the given indice.
+
+        """
+        self.cards[indice] = value
+
+    def __str__(self):
+        """
+        The str magic method.
+
+        :returns: A str of the names of the cards in the deck.
+
+        """
+        card_names = "".join([x.name + "\n" for x in self.cards])
+        return '%s' % (card_names)
 
     def build(self, jokers=False, num_jokers=2):
         """
         Builds a standard 52 card French deck of card objects.
+
+        :param jokers: Whether or not to include jokers in the deck.
+        :type jokers: bool.
+        :param num_jokers: The number of jokers to include.
+        :type num_jokers: int.
 
         :returns: A new deck object.
 
@@ -73,45 +234,30 @@ class Deck(object):
         self.decks_used += 1
 
         temp_deck = [
-            Card(number, suit) for number in self.NUMBERS for 
-            suit in self.SUITS
+            Card(number, suit) for number in NUMBERS for 
+            suit in SUITS
         ]
         temp_deck.extend(
             [
-                Card(face, suit) for face in self.FACES for 
-                suit in self.SUITS
+                Card(face, suit) for face in FACES for 
+                suit in SUITS
             ]
         )
-        temp_deck.extend([Card("Ace", suit) for suit in self.SUITS])
+        temp_deck.extend([Card("Ace", suit) for suit in SUITS])
 
         if jokers:
             temp_deck.extend([Card("Joker", None) for i in xrange(num_jokers)])
 
         self.cards = temp_deck[::-1]
 
-    def check_term(self, card, term):
-        """
-        Checks a given search term against a given card's name attributes.
-
-        :returns: True/False, depending on the check.
-
-        """
-        check_list = [x.lower() for x in
-            card.name, card.suit, card.value, card.abbrev[0],
-            card.abbrev[1], card.abbrev[0:2]
-        ]
-
-        term = term.lower()
-
-        for check in check_list:
-            if check == term:
-                return True
-        else:
-            return False
-
     def deal(self, num=1, rebuild=True):
         """
         Returns a list of cards, which are removed from the deck.
+
+        :param num: The number of cards to deal.
+        :type num: int.
+        :param rebuild: Whether or not to rebuild the deck when cards run out.
+        :type rebuild: bool.
 
         :returns: A given number of cards from the deck.
 
@@ -136,6 +282,10 @@ class Deck(object):
         Searches the deck for cards with a suit, value, name, or
         abbreviation matching the given argument, 'terms'.
 
+        :param terms: The search terms. Can be card full names, suits, values,
+            or abbreviations.
+        :type terms: str|list of str.
+
         :returns: A list of deck indices for the cards matching the given terms, 
             if found.
 
@@ -145,14 +295,23 @@ class Deck(object):
         
         found_cards = [
             self.cards.index(x) for x in self.cards for y in terms if 
-            self.check_term(x, y) 
+            check_term(x, y) 
         ]
 
-        return found_cards
+        if len(found_cards) > 1:
+            return found_cards
+        else:
+            return found_cards[0]
+
+        # return found_cards
 
     def get(self, terms):
         """
         Get the specified cards from the deck.
+
+        :param terms: The search terms. Can be card full names, suits, values,
+            abbreviations, or deck indices.
+        :type terms: str|int|list of str|int.
 
         :returns: A list of the specified cards, if found.
 
@@ -163,41 +322,72 @@ class Deck(object):
             terms = [terms]
             
         for item in terms:
-            index = self.find(item)
-            if index:
-                got_cards.extend(
-                    [self.cards[i] for i in index if 
-                    self.cards[i] not in got_cards]
-                )
+            if type(item) is not int:
+                indices = self.find(item)
+                indices_type = type(indices)
+                if indices_type is list:
+                    got_cards.extend(
+                        [
+                            self.cards[i] for i in indices if 
+                            self.cards[i] not in got_cards
+                        ]
+                    )
+                else:
+                    got_cards.append(self.cards[indices])
+            else:
+                got_cards.append(self.cards[item])
 
         for card in got_cards:
+            print card
             self.cards.remove(card)
 
-        return got_cards
+        if len(got_cards) > 1:
+            return got_cards
+        else:
+            return got_cards[0]
 
-    def peek(self, position, ret_value="card"):
+        # return got_cards
+
+    def peek(self, indice, ret_val="card"):
         """
-        Peek at the card in the given position (list index) of the deck.
+        Peek at the card in the given indice(s) (list index) of the deck.
 
-        :returns: The card at the given deck index.
+        :param indice: The deck indice(s) to peek at.
+        :type indice: int|list of ints.
+        :param ret_value: What to return.
+        :type ret_value: str.
+
+        :returns: The Card|name|suit|value|abbrev. at the given indice.
 
         """
-        return_vals = {
-            "name": self.cards[position].name,
-            "abbr": self.cards[position].abbrev,
-            "abbrev": self.cards[position].abbrev,
-            "abbreviation": self.cards[position].abbrev,
-            "value": self.cards[position].value,
-            "face": self.cards[position].value,
-            "suit": self.cards[position].suit,
-            "card": self.cards[position]
-        }
+        peeked = []
 
-        return return_vals[ret_value]
+        if type(indice) is not list:
+            indice = [indice]
+
+        if ret_val is "card":
+            for i in indice:
+                peeked.append(self.cards[i])
+        elif ret_val is "name":
+            for i in indice:
+                peeked.append(self.cards[i].name)
+        elif ret_val in ["abbr", "abbrev", "abbreviation"]:
+            for i in indice:
+                peeked.append(self.cards[i].abbrev)
+        elif ret_val in ["val", "value"]:
+            for i in indice:
+                peeked.append(self.cards[i].value)
+        elif ret_val in "suit":
+            for i in indice:
+                peeked.append(self.cards[i].suit)
+
+        if len(peeked) > 1:
+            return peeked
+        else:
+            return peeked[0]
 
     def shuffle(self):
         """Shuffles the deck."""
-
         random.shuffle(self.cards)
 
     @property
@@ -208,10 +398,10 @@ class Deck(object):
         :returns: The number of cards in the deck.
 
         """
-        return len(self.cards)
+        return len(self)
 
 #===============================================================================
-# CARD CLASS
+# Card Class
 #===============================================================================
 
 class Card(object):
@@ -219,41 +409,133 @@ class Card(object):
 
     def __init__(self, value, suit):
         """Card constructor method."""
-
         self.value = value
         self.suit = str(suit).capitalize()
         self.abbrev = self.gen_abbrev(value, suit)
         self.name = self.gen_name(value, suit)
+
+    def __eq__(self, other):
+        """
+        Allows for Card value/suit equality comparisons.
+
+        :param other: The other card to compare to.
+        :type other: Card.
+
+        :returns: Whether or not the cards are equal.
+
+        """
+        return (
+            isinstance(other, Card) and self.value == other.value and
+            self.suit == other.suit
+        )
+
+    def __gt__(self, other):
+        """
+        Allows for Card value comparisons. Uses poker ranks.
+
+        :param other: The other card to compare to.
+        :type other: Card.
+
+        :returns: Whether or not self > other.
+
+        """
+        return POKER_RANKS[self.value] > POKER_RANKS[other.value]
+
+    def __hash__(self):
+        """
+        The hash magic method.
+
+        :returns: A unique number, or hash for the Card.
+
+        """
+        return hash((self.value, self.suit))
+
+    def __repr__(self):
+        """
+        The repr magic method.
+
+        :returns: A string representation of the Card object.
+
+        """
+        return "Card(value=%r, suit=%r)" % (self.value, self.suit)
+
+    def __str__(self):
+        """
+        The str magic method.
+
+        :returns: The card name.
+
+        """
+        return "%s" % (self.name)
 
     def gen_abbrev(self, value, suit):
         """
         Constructs an abbreviation for the card, using the given 
         value, and suit.
 
-        :returns: A newly constructed abbreviation, made using the given value
+        :param value: The value to use.
+        :type value: str.
+        :param suit: The suit to use.
+        :type suit: str.
+
+        :returns: A newly constructed abbreviation, using the given value
             & suit
 
         """
-        if value == "Joker":
+        if value is "Joker":
             return "JK"
-        elif type(value) is str:
-            return "%s%s" % (value[0], suit[0])
-        elif type(value) is int:
-            return "%s%s" % (str(value), suit[0])
-            
+        elif value is "10":
+            return "10%s" % (suit[0])
+        else:
+            return "%s%s" % (value[0], suit[0]) 
 
     def gen_name(self, value, suit):
         """
         Constructs a name for the card, using the given value, 
         and suit.
 
+        :param value: The value to use.
+        :type value: str.
+        :param suit: The suit to use.
+        :type suit: str.
+
         :returns: a newly constructed name, using the given value & suit.
 
         """
-        if value == "Joker":
+        if value is "Joker":
             return "Joker"
         else:
             return "%s of %s" % (str(value), suit)
+
+#===============================================================================
+# Other Functions
+#===============================================================================
+
+def check_term(card, term):
+    """
+    Checks a given search term against a given card's name attributes.
+
+    :param card: The card to check.
+    :type card: Card.
+    :param term: The search term to check for. Can be a card full name, suit, 
+        value, or abbreviation.
+    :type terms: str.
+
+    :returns: True/False, depending on the check.
+
+    """
+    check_list = [
+        x.lower() for x in
+        card.name, card.suit, card.value, card.abbrev,
+        card.abbrev[0], card.abbrev[1]
+    ]
+
+    term = term.lower()
+
+    for check in check_list:
+        if check == term:
+            return True
+    return False
 
 #===============================================================================
 # IF '__MAIN__'
